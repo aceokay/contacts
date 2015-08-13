0274d7c9d5653ff059d439b78e455984ff954c4a
Josue Valverde & Austin Kincaid <student@epicodus.com>
2015-08-13 12:11:57 -0700

    Add app.rb, Gemfile, and basic homepage

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -7,6 +7,9 @@ class Contact
     @job_title = attributes.fetch(:job_title)
     @company = attributes.fetch(:company)
     @id = @@contacts.length() + 1
+    @phone_numbers = []
+    @emails = []
+    @addresses = []
   end
 
   define_method(:first_name) do
@@ -21,6 +24,18 @@ class Contact
     @@contacts.push(self)
   end
 
+  define_method(:add_number) do |number|
+    @phone_numbers.push(number)
+  end
+
+  define_method(:add_email) do |email|
+    @emails.push(email)
+  end
+
+  define_method(:add_address) do |address|
+    @addresses.push(address)
+  end
+
   define_singleton_method(:clear) do
     @@contacts = []
   end
@@ -32,4 +47,5 @@ class Contact
   define_singleton_method(:find) do |id|
     @@contacts[id - 1]
   end
+
 end
