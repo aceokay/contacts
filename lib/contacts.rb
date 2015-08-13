4787f3d3c15b7fdee4365febb09c7e4e96c0c4d2
Josue Valverde & Austin Kincaid <student@epicodus.com>
2015-08-13 09:27:00 -0700

    Add method call to return all attributes

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,2 +1,9 @@
 class Contact
+  attr_reader(:name, :job_title, :company)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @job_title = attributes.fetch(:job_title)
+    @company = attributes.fetch(:company)
+  end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -4,8 +4,8 @@ require('contacts')
 describe(Contact) do
   describe('#first_name') do
     it('returns the first name of the contact') do
-      test_contact = Contact.new({:first_name => "Austin", :last_name => "Kincaid", :job_title => "Senior Developer", :company => "Acme"})
-      expect(test_contact.first_name()).to(eq("Austin"))
+      test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
+      expect(test_contact.name()).to(eq("Austin Kincaid"))
     end
   end
 end
