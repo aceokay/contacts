7a43095c2ee8ca45fe71b228c17989e6743b71ea
Josue Valverde & Austin Kincaid <student@epicodus.com>
2015-08-13 09:34:02 -0700

    add and pass specs that return first and last name of contact.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -6,4 +6,12 @@ class Contact
     @job_title = attributes.fetch(:job_title)
     @company = attributes.fetch(:company)
   end
+
+  define_method(:first_name) do
+    @name.split(" ")[0]
+  end
+
+  define_method(:last_name) do
+    @name.split(" ").last()
+  end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -23,4 +23,18 @@ describe(Contact) do
     end
   end
 
+  describe('#first_name') do
+    it('returns the first name of contact') do
+      test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
+      expect(test_contact.first_name()).to(eq("Austin"))
+    end
+  end
+
+  describe('#last_name') do
+    it('returns the last name of contact') do
+      test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
+      expect(test_contact.last_name()).to(eq("Kincaid"))
+    end
+  end
+
 end
