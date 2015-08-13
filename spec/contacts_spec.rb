024392377a699bd2551b8f85964606d5cdd4bfb5
Josue Valverde & Austin Kincaid <student@epicodus.com>
2015-08-13 09:29:47 -0700

    add and pass all specs that return contact attributes.

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -2,10 +2,25 @@ require('rspec')
 require('contacts')
 
 describe(Contact) do
-  describe('#first_name') do
+  describe('#name') do
     it('returns the first name of the contact') do
       test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
       expect(test_contact.name()).to(eq("Austin Kincaid"))
     end
   end
+
+  describe('#job_title') do
+    it('returns the job title of the contact') do
+      test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
+      expect(test_contact.job_title()).to(eq("Senior Developer"))
+    end
+  end
+
+  describe('#company') do
+    it('returns the company of the contact') do
+      test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
+      expect(test_contact.company()).to(eq("Acme"))
+    end
+  end
+
 end
