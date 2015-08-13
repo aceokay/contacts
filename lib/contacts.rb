d319855246b72e59805069ace735152ccdf8ce96
Josue Valverde & Austin Kincaid <student@epicodus.com>
2015-08-13 09:53:13 -0700

    add and pass specs that return all contacts, clear all contacts, save a contact, and return an id.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,10 +1,12 @@
 class Contact
-  attr_reader(:name, :job_title, :company)
+  attr_reader(:name, :job_title, :company, :id)
+  @@contacts = []
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @job_title = attributes.fetch(:job_title)
     @company = attributes.fetch(:company)
+    @id = @@contacts.length() + 1
   end
 
   define_method(:first_name) do
@@ -13,5 +15,17 @@ class Contact
 
   define_method(:last_name) do
     @name.split(" ").last()
+  end
+
+  define_method(:save) do
+    @@contacts.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@contacts = []
+  end
+
+  define_singleton_method(:all) do
+    @@contacts
   end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -2,6 +2,11 @@ require('rspec')
 require('contacts')
 
 describe(Contact) do
+
+  before() do
+    Contact.clear()
+  end
+
   describe('#name') do
     it('returns the first name of the contact') do
       test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
@@ -37,4 +42,40 @@ describe(Contact) do
     end
   end
 
+  describe('#save') do
+    it('saves a given contact to the contact array') do
+      test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
+      test_contact.save()
+      expect(Contact.all()).to(eq([test_contact]))
+    end
+  end
+
+  describe('.all') do
+    it('returns all contacts in the Contact class array') do
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties the Contact class array') do
+      test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
+      test_contact.save()
+      test_contact2 = Contact.new({:name => "Josue Valverde", :job_title => "Senior Developer", :company => "Acme"})
+      test_contact2.save()
+      expect(Contact.clear()).to(eq([]))
+      end
+  end
+
+  describe('#id') do
+    it('returns the id of a contact') do
+      test_contact = Contact.new({:name => "Austin Kincaid", :job_title => "Senior Developer", :company => "Acme"})
+      test_contact.save()
+      test_contact2 = Contact.new({:name => "Josue Valverde", :job_title => "Senior Developer", :company => "Acme"})
+      test_contact2.save()
+      test_contact3 = Contact.new({:name => "Mike Bunker", :job_title => "Senior Developer", :company => "Acme"})
+      test_contact3.save()
+
+      expect(test_contact2.id()).to(eq(2))
+    end
+  end
 end
